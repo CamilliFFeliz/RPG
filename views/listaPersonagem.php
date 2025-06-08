e01297e86e0c431078f0e8ffb410116482d113e0
BlueKannary <132022245+BlueKannary@users.noreply.github.com>
2025-06-08 18:27:27 -0300

    Add files via upload

--- views/listaPersonagem.php
+++ views/listaPersonagem.php
@@ -1,15 +1,17 @@
 <?php
-$personagens = PersonagemDAO::listar(); // deve ser PersonagemController->listar()
+$personagens = PersonagemDAO::listar(); 
 ?>
 
-<h1>Lista de Personagens</h1>
+<div class="container">
+    <h1>Lista de Personagens</h1>
 
-<?php if (count($personagens) > 0): ?>
-    <ul>
-        <?php foreach ($personagens as $personagem): ?>
-            <li><?= htmlspecialchars($personagem['nome']) ?></li>
-        <?php endforeach; ?>
-    </ul>
-<?php else: ?>
-    <p>Nenhum personagem cadastrado.</p>
-<?php endif; ?>
+    <?php if (count($personagens) > 0): ?>
+        <ul class="personagem-lista">
+            <?php foreach ($personagens as $personagem): ?>
+                <li class="personagem-card"><?= htmlspecialchars($personagem['nome']) ?></li>
+            <?php endforeach; ?>
+        </ul>
+    <?php else: ?>
+        <p>Nenhum personagem cadastrado.</p>
+    <?php endif; ?>
+</div>
